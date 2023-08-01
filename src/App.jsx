92b747409a538e068a3766d4a209cc5cec2a552c
Ahmed <himozhima@gmail.com>
2023-08-01 10:28:28 +0300

    implemnting nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
-import Pricing from "./pages/pricing";
+import Pricing from "./pages/Pricing";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route index element = {<p>list of cities</p>} />
+            <Route path="cities" element={<p>List of cities</p>} />
+            <Route path="countries" element={<p>countries</p>} />
+            <Route path="form" element={<p>Form</p>} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
